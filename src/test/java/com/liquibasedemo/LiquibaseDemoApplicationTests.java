fccedc05c6ffd9824d964ef5549a40c62e66383a
Ivan Wijaya <ivan.wijaya@mii.co.id>
2022-05-27 13:57:21 +0700

    update project

--- src/test/java/com/liquibasedemo/LiquibaseDemoApplicationTests.java
+++ src/test/java/com/liquibasedemo/LiquibaseDemoApplicationTests.java
@@ -1,13 +1,13 @@
-package com.liquibase;
+package com.liquibasedemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LiquibaseApplicationTests {
+class LiquibaseDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
