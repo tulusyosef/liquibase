bc360b651b39015b49f0f51a9778b40c25f604ce
Tulus Yosef <tulusyosef@gmail.com>
2022-06-08 14:00:39 +0700

    first commit, only working for postgres in my local

--- src/main/resources/liquibase/changeset/create-sql.sql
+++
@@ -1,8 +0,0 @@
-create table person
-(
-    id       int auto_increment,
-    name     varchar(100)  not null,
-    feedback varchar(1000) null,
-    constraint person_pk
-        primary key (id)
-);
---
+++ src/main/resources/liquibase/changeset/create1.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS category1mvn
+(
+    category_id integer NOT NULL,
+    name character varying(25) NOT NULL
+)
