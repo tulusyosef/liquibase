719a0eb88f5b44295cb5f37ebf65b5d2964b7f03
Tulus Yosef <tulusyosef@gmail.com>
2022-06-13 08:30:56 +0700

    modify sql changeset

--- src/main/resources/liquibase/changeset/0040_insert_category4mvn.sql
+++ src/main/resources/liquibase/changeset/0040_insert_category4mvn.sql
@@ -1,5 +1,3 @@
 -- insert 0040
 
-INSERT INTO public.category4mvn(
-	category_id, name)
-	VALUES (4, tty4);
+INSERT INTO public.category4mvn(category_id, name) VALUES (4, 'tty4');
--- target/classes/liquibase/changeset/0030_insert_category3mvn.sql
+++ target/classes/liquibase/changeset/0030_insert_category3mvn.sql
@@ -1,5 +1,3 @@
 -- insert 0030
 
-INSERT INTO public.category3mvn(
-	category_id, name)
-	VALUES (3, tty3);
+INSERT INTO public.category3mvn(category_id, name) VALUES (3, 'tty3');
